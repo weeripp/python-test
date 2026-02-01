ab58162053cad81622e85418ad1971a2c76208b8
weeripp <93368033+weeripp@users.noreply.github.com>
2026-02-01 21:07:33 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -6,6 +6,13 @@ from selenium.webdriver.firefox.options import Options
 def browser():
     options = Options()
     options.add_argument('--headless')
+    
+    # Отключаем использование прокси
+    options.set_preference('network.proxy.type', 0)
+    
+    # Также можно явно задать через capabilities
+    browser = webdriver.Firefox(options=options)
+    
     browser = webdriver.Firefox(options=options)
     browser.maximize_window()
     browser.implicitly_wait(3)
